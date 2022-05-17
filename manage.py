c51d38b783147ccf95f7dcd86f7d3abdd50c5ed7
Ngina <Nginagitau11@outlook.com>
2022-05-18 00:06:01 +0300

    update: upgrades db with new models

--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 from app import create_app,db
 from flask_script import Manager,Shell,Server
 from flask_migrate import Migrate, MigrateCommand
-from app.models import User, PhotoProfile
+from app.models import User, PhotoProfile, Post, Comment
 
 # app = create_app('development')
 app = create_app('test')
@@ -23,7 +23,7 @@ def test():
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app, db = db, User = User)
+    return dict(app = app, db = db, User = User, Post = Pos, Comments = Comments, PhotoProfile = PhotoProfile)
 
 if __name__ == '__main__':
     manager.run()

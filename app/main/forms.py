4ad4b91d28ddbddfdb2fd9a20001bdea2183d792
Ngina <Nginagitau11@outlook.com>
2022-05-18 00:49:31 +0300

    update: create forms

--- app/main/forms.py
+++ app/main/forms.py
@@ -6,3 +6,16 @@ from ..models import User
 class UpdateProfile(FlaskForm):
     bio = TextAreaField('Tell us about you.',validators = [InputRequired()])
     submit = SubmitField('Submit')
+
+class PostForm(FlaskForm):
+    title = TextAreaField('Title')
+    content = TextAreaField('Write Something')
+    submit = SubmitField('SUBMIT')
+
+class CommentForm(FlaskForm):
+    opinion = TextAreaField('Comment')
+    submit = SubmitField('SUBMIT')
+
+class CategoryForm(FlaskForm):
+    name =  StringField('Category Name', validators=[InputRequired()])
+    submit = SubmitField('Create')

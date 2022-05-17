40f3d75cc91a09d88b21b73776fd262c7c6473d2
Ngina <Nginagitau11@outlook.com>
2022-05-17 23:17:12 +0300

    update: adds forms

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,3 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, SubmitField, ValidationError
 from wtforms.validators import InputRequired, Email
+from ..models import User
+
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Tell us about you.',validators = [InputRequired()])
+    submit = SubmitField('Submit')
